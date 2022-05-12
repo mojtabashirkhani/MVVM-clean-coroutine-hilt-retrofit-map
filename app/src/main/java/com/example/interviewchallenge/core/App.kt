d73b41e16c4b24b164c83eaecf0051a37b3982ce
mojtabashirkhani <mojtabashirkhani.nba@gmail.com>
2022-05-12 11:28:07 +0430

    setup neshan map

--- app/src/main/java/com/example/interviewchallenge/core/App.kt
+++ app/src/main/java/com/example/interviewchallenge/core/App.kt
@@ -1,14 +1,10 @@
 package com.example.interviewchallenge.core
 
 import android.app.Application
-import android.content.Context
-import androidx.multidex.MultiDex
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class App: Application() {
-    override fun attachBaseContext(context: Context) {
-        super.attachBaseContext(context)
-        MultiDex.install(this)
-    }
+
+
 }
--- app/src/main/java/com/example/interviewchallenge/ui/MainActivity.kt
+++ app/src/main/java/com/example/interviewchallenge/ui/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.interviewchallenge.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import com.example.interviewchallenge.R
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.view.*
+import org.neshan.mapsdk.MapView
+import org.neshan.mapsdk.style.NeshanMapStyle
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<MapView>(R.id.map)
+
     }
 }
