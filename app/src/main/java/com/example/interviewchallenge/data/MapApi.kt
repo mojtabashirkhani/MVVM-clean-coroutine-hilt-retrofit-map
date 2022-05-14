2e4aac9ffeaf294be2d1336a96458627abcd43ac
mojtabashirkhani <mojtabashirkhani.nba@gmail.com>
2022-05-14 20:11:12 +0430

    add map retofit services for direction and matrix api

--- app/src/main/java/com/example/interviewchallenge/data/MapApi.kt
+++ app/src/main/java/com/example/interviewchallenge/data/MapApi.kt
@@ -1,8 +1,43 @@
 package com.example.interviewchallenge.data
 
+import org.neshan.servicessdk.direction.model.NeshanDirectionResult
+import org.neshan.servicessdk.distancematrix.model.NeshanDistanceMatrixResult
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
 
 
 interface MapApi {
+
+    @GET("/v2/direction")
+    suspend fun getNeshanDirection(
+        @Header("Api-Key") mapApiKey: String?,
+        @Query("origin") origin: String?,
+        @Query("destination") destination: String?,
+        @Query("waypoints") waypoints: String?,
+        @Query("avoidTrafficZone") avoidTerrificZone: Boolean,
+        @Query("avoidOddEvenZone") avoidOddEvenZone: Boolean,
+        @Query("alternative") alternative: Boolean
+    ): Response<NeshanDirectionResult?>?
+
+    @GET("/v2/direction")
+    suspend fun getNeshanDirection(
+        @Header("Api-Key") mapApiKey: String?,
+        @Query("origin") origin: String?,
+        @Query("destination") destination: String?,
+        @Query("avoidTrafficZone") avoidTerrificZone: Boolean,
+        @Query("avoidOddEvenZone") avoidOddEvenZone: Boolean,
+        @Query("alternative") alternative: Boolean
+    ): Response<NeshanDirectionResult?>?
+
+
+    @GET("/v1/distance-matrix")
+    suspend fun getNeshanDistanceMatrix(
+        @Header("Api-Key") mapApiKey: String?,
+        @Query("origins") origin: String?,
+        @Query("destinations") destination: String?
+    ): Response<NeshanDistanceMatrixResult?>?
 
 
 }
---
+++ app/src/main/java/com/example/interviewchallenge/ui/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.interviewchallenge.ui
+
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+
+class MainViewModel {
+}
